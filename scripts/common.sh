c8ff2eebbab7d8ac2807473cb5defc3faa1371ca
Tobias Waslowski <tobiaswaslowski@gmail.com>
2024-12-16 18:21:13 +0100

    feat: create working helm deployment

--- scripts/common.sh
+++ scripts/common.sh
@@ -12,14 +12,36 @@ function package() {
 }
 
 function deploy() {
-  TAG="sha-$(git rev-parse --short HEAD)"
+  if [ -z "$TAG" ]; then
+    TAG="sha-$(git rev-parse --short HEAD)"
+  fi
 
-  export TF_VAR_image_tag="$TAG"
-  export TF_VAR_telegram_token="$TELEGRAM_TOKEN"
+  if [ -z "$OPENAI_API_KEY" ] || [ -z "$DATASOURCE_PASSWORD" ] || [ -z "$TELEGRAM_TOKEN_PROD" ]; then
+    echo "Please set OPENAI_API_KEY, DATASOURCE_PASSWORD and TELEGRAM_TOKEN_PROD environment variables"
+    exit 1
+  fi
 
-  pushd terraform || exit
-  terraform init
-  terraform apply -auto-approve
+  helm upgrade --install \
+    --set global.postgresql.auth.password="$DATASOURCE_PASSWORD" \
+    --values ./charts/values/postgres-values.yaml \
+    --namespace grammr --create-namespace \
+    --wait --timeout 60 \
+    postgres oci://registry-1.docker.io/bitnamicharts/postgresql
+
+  helm upgrade --install \
+    --set image.tag=latest \
+    --set spacyModels=ru_core_news_sm \
+    --namespace grammr --create-namespace \
+    --wait --timeout 60 \
+    grammr-morphology ./charts/sidecar-analysis
+
+    helm upgrade --install \
+      --set openai_api_key="$OPENAI_API_KEY" \
+      --set telegram_token="$TELEGRAM_TOKEN_PROD" \
+      --set datasource_password="$DATASOURCE_PASSWORD" \
+      --set image.tag="$TAG" \
+      --namespace grammr --create-namespace \
+      grammr-core ./charts/grammr
 }
 
 function activate_minikube() {
