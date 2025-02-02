9f927b1fc45282b6b168661dedb87e65059cc13f
Tobias Waslowski <tobiaswaslowski@gmail.com>
2025-02-02 15:50:31 +0100

    feat: add cors configuration

--- src/main/java/com/grammr/config/SpringSecurityConfiguration.java
+++ src/main/java/com/grammr/config/SpringSecurityConfiguration.java
@@ -2,15 +2,22 @@ package com.grammr.config;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.NEVER;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SpringSecurityConfiguration {
+
+  @Value("${spring.cors.allowed-origin}")
+  private String allowedOrigin;
 
   @Bean
   public SecurityFilterChain configureWebSecurity(HttpSecurity httpSecurity) throws Exception {
@@ -20,6 +27,16 @@ public class SpringSecurityConfiguration {
         .authorizeHttpRequests(this::configureRestAuthorizations)
         .sessionManagement(session -> session.sessionCreationPolicy(NEVER))
         .build();
+  }
+
+  @Bean
+  public UrlBasedCorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(List.of(allowedOrigin));
+    configuration.setAllowedMethods(List.of("GET", "POST"));
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 
   private void configureRestAuthorizations(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry authorizationRegistry) {
