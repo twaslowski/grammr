86c7cdea98a4605958433bee2ff0ee156bd8908a
Tobias Waslowski <tobiaswaslowski@gmail.com>
2025-03-05 18:40:14 +0100

    chore: re-enable core deployment workflow
    also provide more detailed logging in multi-inflection service

--- sidecars/multi-inflection/inflection/service/inflector.py
+++ sidecars/multi-inflection/inflection/service/inflector.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 from verbecc import Conjugator
@@ -7,17 +8,21 @@ from verbecc.localization import xtense, xmood
 from inflection.domain.inflection import Inflection
 from inflection.service import feature_retriever
 
+
+logger = logging.getLogger(__name__)
 LANG = os.environ["LANGUAGE_CODE"]
 
 if LANG not in ["it", "fr", "es", "pt", "ro"]:
     raise ValueError("Language not supported")
 
+logger.info(f"Downloading model for language: {LANG}")
 conjugator = Conjugator(lang=LANG)
 
 
 def inflect(lemma: str) -> list[Inflection]:
     features = feature_retriever.retrieve_features()
     inflections = xconj(lemma)
+    logger.info(f"Inflections for lemma {lemma}: {inflections}")
     result = []
     for feature, inflected in zip(features, inflections):
         result.append(Inflection(lemma=lemma, inflected=inflected, features=feature))
--- sidecars/multi-inflection/inflection/service/stringifier.py
+++
@@ -1,7 +0,0 @@
-import json
-
-from inflection.domain.inflection import Inflection
-
-
-def stringify_inflections(inflections: list[Inflection]) -> str:
-    return json.dumps([inflection.model_dump() for inflection in inflections])
