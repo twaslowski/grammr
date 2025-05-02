0cc118ddc4af8d95babae2dcf1ac8015db43db18
Tobias Waslowski <tobiaswaslowski@gmail.com>
2025-05-02 18:58:37 +0200

    fix(inflections): send fullIdentifier to frontend

--- src/main/java/com/grammr/domain/enums/features/Feature.java
+++ src/main/java/com/grammr/domain/enums/features/Feature.java
@@ -1,7 +1,10 @@
 package com.grammr.domain.enums.features;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record Feature(FeatureType type, String value) {
 
   @JsonIgnore
@@ -15,6 +18,11 @@ public record Feature(FeatureType type, String value) {
       case ANIMACY -> Animacy.valueOf(value);
       case MISC -> MiscellaneousFeature.UNSUPPORTED_FEATURE;
     };
+  }
+
+  @JsonProperty("fullIdentifier")
+  public String fullIdentifier() {
+    return getEnumValue().fullIdentifier();
   }
 
   public static Feature of(FeatureType type, String value) {
