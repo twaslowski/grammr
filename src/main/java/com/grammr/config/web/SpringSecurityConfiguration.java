01392e18e95666cd2f46743fc9c5c6f8802153f0
Tobias Waslowski <tobiaswaslowski@gmail.com>
2025-05-25 17:57:38 +0200

    fix(chat): cors configuration

--- src/main/java/com/grammr/config/web/SpringSecurityConfiguration.java
+++ src/main/java/com/grammr/config/web/SpringSecurityConfiguration.java
@@ -2,10 +2,7 @@ package com.grammr.config.web;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.NEVER;
 
-import jakarta.validation.constraints.NotNull;
-import java.util.List;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -16,17 +13,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Slf4j
 @Configuration
 public class SpringSecurityConfiguration {
-
-  @Value("${spring.cors.allowed-origin}")
-  private String allowedOrigin;
 
   @Bean
   public SecurityFilterChain configureWebSecurity(HttpSecurity httpSecurity,
@@ -39,32 +29,6 @@ public class SpringSecurityConfiguration {
         .addFilterBefore(clerkJwtValidationFilter, UsernamePasswordAuthenticationFilter.class)
         .sessionManagement(session -> session.sessionCreationPolicy(NEVER))
         .build();
-  }
-
-  @Bean
-  public UrlBasedCorsConfigurationSource corsConfigurationSource() {
-    CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(List.of(allowedOrigin));
-    configuration.setAllowedMethods(List.of("GET", "POST"));
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", configuration);
-    return source;
-  }
-
-  //   todo: this needs to go, but the above cors configuration is not working
-  @Bean
-  public WebMvcConfigurer corsConfigurer() {
-    return new WebMvcConfigurer() {
-      @Override
-      public void addCorsMappings(@NotNull CorsRegistry registry) {
-        registry.addMapping("/api/**")
-            .allowedOrigins(allowedOrigin)
-            .allowedMethods("GET", "POST", "OPTIONS", "DELETE")
-            .allowedHeaders("*")
-            .allowCredentials(true)
-            .maxAge(3600); // 1 hour
-      }
-    };
   }
 
   private void configureRestAuthorizations(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry authorizationRegistry) {
