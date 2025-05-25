ee9fdd6da328761735d3963b4ef899ea22e938f7
Tobias Waslowski <tobiaswaslowski@gmail.com>
2025-05-25 19:24:11 +0200

    fix(chat): annotate ChatController as crossOrigin

--- src/main/java/com/grammr/controller/ChatController.java
+++ src/main/java/com/grammr/controller/ChatController.java
@@ -14,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 @Slf4j
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1/chat")
 @RequiredArgsConstructor
