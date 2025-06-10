eba37756d98253e55ac84efb2af262b6d449796a
Tobias Waslowski <tobiaswaslowski@gmail.com>
2025-06-10 17:51:09 +0300

    feat(tts): ensure high-quality text-to-speech output

--- terraform/application/lambda/tts/polly.py
+++ terraform/application/lambda/tts/polly.py
@@ -3,29 +3,59 @@ import boto3
 import json
 
 languages = {
-  'RU': 'ru-RU',
-  'EN': 'en-US',
-  'FR': 'fr-FR',
-  'DE': 'de-DE',
-  'IT': 'it-IT',
-  'ES': 'es-ES',
-  'PT': 'pt-PT',
+  'RU': {
+    'languageCode': 'ru-RU',
+    'voiceId': 'Tatyana',
+    'engine': 'standard'
+  },
+  'EN': {
+    'languageCode': 'en-US',
+    'voiceId': 'Danielle',
+    'engine': 'generative',
+  },
+  'FR': {
+    'languageCode': 'fr-FR',
+    'voiceId': 'Lea',
+    'engine': 'generative'
+  },
+  'DE': {
+    'languageCode': 'de-DE',
+    'voiceId': 'Vicki',
+    'engine': 'generative'
+  },
+  'IT': {
+    'languageCode': 'it-IT',
+    'voiceId': 'Carla',
+    'engine': 'generative'
+  },
+  'ES': {
+    'languageCode': 'es-ES',
+    'voiceId': 'Lucia',
+    'engine': 'generative'
+  },
+  'PT': {
+    'languageCode': 'pt-PT',
+    'voiceId': 'Ines',
+    'engine': 'neural'
+  },
 }
-
 
 def lambda_handler(event, context):
   try:
     data = json.loads(event['body'])
     text = data['text']
-    language = data.get('language', 'EN').upper()
-    voice = data.get('voice', 'Joanna')
+    language = data.get('language', 'EN')
+    language_code = languages.get(language, languages['EN'])['languageCode']
+    voice = languages.get(language, languages['EN'])['voiceId']
+    engine = languages.get(language, languages['EN'])['engine']
 
     polly_client = boto3.client('polly')
     response = polly_client.synthesize_speech(
         Text=text,
         OutputFormat='mp3',
-        LanguageCode=languages[language],
-        VoiceId=voice
+        LanguageCode=language_code,
+        VoiceId=voice,
+        Engine=engine,
     )
 
     audio_stream = base64.b64encode(response['AudioStream'].read()).decode(
