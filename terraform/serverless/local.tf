b911ce542c7b5a1baafeeb6f4112194c7f2bcd2a
Tobias Waslowski <contact@twaslowski.com>
2025-06-30 18:13:13 +0200

    chore: entirely migrate environments/ folder into terraform config; change sidecar workflows to build only

--- terraform/serverless/local.tf
+++ terraform/serverless/local.tf
@@ -1,5 +1,5 @@
 locals {
-  config                = yamldecode(file("${path.module}/../../environments/${var.environment}/morphology.values.yaml"))
+  config                = yamldecode(file("${path.module}/config/values/${var.environment}/morphology.values.yaml"))
   languages_map = {
     for lang in local.config.languages : lang.code => lang
   }
