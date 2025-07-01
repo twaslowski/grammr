5e66b4a4151bb4fb7fd197b223868c4932b46e84
Tobias Waslowski <contact@twaslowski.com>
2025-07-01 16:25:48 +0200

    chore: bump chart versions

--- terraform/application/sidecars.tf
+++ terraform/application/sidecars.tf
@@ -3,7 +3,7 @@ resource "helm_release" "morphology_ru" {
 
   repository = "https://twaslowski.github.io/grammr-charts"
   chart      = "inflection-ru"
-  version    = "0.2.2"
+  version    = "0.2.3"
 
   namespace = kubernetes_namespace_v1.namespace.metadata[0].name
   timeout   = 300
@@ -14,7 +14,7 @@ resource "helm_release" "multi_inflection" {
   name       = "multi-inflection"
   chart      = "multi-inflection"
   repository = "https://twaslowski.github.io/grammr-charts"
-  version    = "0.1.4"
+  version    = "0.2.2"
 
   values = [
     file("${path.module}/config/values/${var.environment}/multi-inflection.values.yaml")
@@ -29,7 +29,7 @@ resource "helm_release" "anki_exporter" {
   name       = "anki-exporter"
   chart      = "anki-exporter"
   repository = "https://twaslowski.github.io/grammr-charts"
-  version    = "0.1.2"
+  version    = "0.2.0"
 
   namespace = kubernetes_namespace_v1.namespace.metadata[0].name
   timeout   = 300
