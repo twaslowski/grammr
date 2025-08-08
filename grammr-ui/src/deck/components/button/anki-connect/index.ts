beb23f1b98ca70c91545c156c1fa1bcc02044276
Tobias Waslowski <contact@twaslowski.com>
2025-08-08 18:09:53 +0200

    feat(anki): create flashcard deletion process

--- grammr-ui/src/deck/components/button/anki-connect/index.ts
+++ grammr-ui/src/deck/components/button/anki-connect/index.ts
@@ -1,4 +1,4 @@
 // Re-export all anki-connect functions
 export { createDeck } from './deck';
 export { precheckAnkiConnect } from './util';
-export { getNote, createNotes, updateNote, deleteNotes } from './notes';
+export { findNoteByFront, createNotes, updateNote, updateNotes, deleteNotes } from './notes';
